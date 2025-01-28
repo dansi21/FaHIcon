9156f28754231fb8466bb1b2678d7c85b09a3863
willg <willg@ONLYFOREVE>
2025-01-27 21:23:17 -0800

    Add project files.

---
+++ FaHIconAPI/Controllers/IconController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FaHIconAPI.Controllers
+{
+    [ApiController]
+    public class IconController : Controller
+    {
+        [HttpGet]
+        [Route("/icon/leaderboard/{userId}")]
+        public IActionResult GetLeaderboardIcon(string userId)
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ FaHIconAPI/Models/UserData.cs
@@ -0,0 +1,10 @@
+﻿namespace FaHIconAPI.Models
+{
+    public class UserData
+    {
+        public string UserId { get; set; } = "";
+        public string UserName { get; set; } = "";
+        public long Points { get; set; } = 0;
+        public int Rank { get; set; } = 0;
+    }
+}
