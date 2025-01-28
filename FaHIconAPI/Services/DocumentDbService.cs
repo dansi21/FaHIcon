fff429c6f97147e95b11eb0a2a6104c287c5ff6d
William Lathrop <willguy21@gmail.com>
2025-01-27 22:03:04 -0800

    Added Planned Service Signatures

---
+++ FaHIconAPI/Services/DocumentDbService.cs
@@ -0,0 +1,21 @@
+﻿using FaHIconAPI.Models;
+
+namespace FaHIconAPI.Services
+{
+    public class DocumentDbService
+    {
+        public DocumentDbService()
+        {
+        }
+
+        public Task<UserData> GetUserData(string userId)
+        {
+            return Task.FromResult(new UserData());
+        }
+
+        public Task<UserData> UpdateUserData(UserData userData)
+        {
+            return Task.FromResult(userData);
+        }
+    }
+}
---
+++ FaHIconAPI/Services/FaHApiService.cs
@@ -0,0 +1,16 @@
+﻿using FaHIconAPI.Models;
+
+namespace FaHIconAPI.Services
+{
+    public class FaHApiService
+    {
+        public FaHApiService()
+        {
+        }
+
+        public Task GetUserData()
+        {
+            return Task.FromResult(new UserData());
+        }
+    }
+}
